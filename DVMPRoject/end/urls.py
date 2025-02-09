e3ad6563972c4b60ff878430fdeacdeb2353ae6b
eshs01 <f20241262@pilani.bits-pilani.ac.in>
2025-02-10 01:46:17 +0530

    #3 commit
    a lot changed

---
+++ DVMPRoject/end/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .import views
+urlpatterns =[
+    path("",views.loginuser,name="login"),
+    path("logout",views.logoutuser,name="logout"),
+    path("home",views.home,name="home"),
+    path("<int:bus_id>/",views.details,name="bus"),
+    
+   
+
+
+]
---
+++ DVMPRoject/end/views.py
@@ -0,0 +1,96 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponseRedirect,HttpResponse
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from .models import Bus,Book
+from django.shortcuts import get_object_or_404
+# Create your views here
+def home(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+
+
+    return render(request,"end/home.html",{
+        "bus":Bus.objects.all()
+    })
+
+def loginuser(request):
+    if request.method == "POST":
+        username = request.POST.get("username")  # Use .get() to avoid KeyError
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("home"))
+        else:
+            return render(request, "end/login.html", {
+                "message": "Invalid credentials",
+            })
+
+
+    return render(request, "end/login.html")
+#needed to get http response
+
+
+def logoutuser(request):
+    logout(request)
+    return render(request,"end/login.html",{
+        "message":"logged out"
+    })
+
+    
+def details(request,bus_id):
+    #bus=Bus.objects.get(id=bus_id)
+    #save the details of specific Bus in bus
+    #return render(request,"end/bus.html",{
+    #   "bus":bus
+        #passing bus(line38) to html in form of bus 
+       # i joined the book and and the detail function as both of them werer not working seperately
+    bus =  Bus.objects.get(id=bus_id)
+    if request.method == "POST":
+        # Get the number of seats requested
+        bookseats = int(request.POST.get("bookseats", 0))
+        
+        
+        if bus.availableseats:
+            if bookseats > bus.availableseats:
+                return render(request, "end/bus.html", {
+                    "bus": bus,
+                    "message": "Not that many seats available",
+                })
+        bus.availableseats -= bookseats
+        bus.save()
+        
+
+        
+        return render(request, "end/bus.html", {
+            "bus": bus,
+            "message": f"Thank you for booking {bookseats} seat(s).",
+        })
+    return render(request, "end/bus.html", {"bus": bus})
+    
+"""
+def book(request, bus_id):
+    if request.method == "POST":
+        bookseats = request.POST.get("no_of_seats")
+        if not bookseats:
+            return render(request, "end/bus.html",{
+                "message": "Invalid input"
+            })
+        
+        bookseats=int(bookseats)
+
+        bus=Bus.objects.get(id=bus_id)
+        if bookseats > bus.availableseats:
+            return render(request, "end/bus.html", {
+                "message": "Not enough seats available"
+            })
+
+        bus.availableseats-= bookseats
+        bus.save()
+        
+        return render(request,"end/bus.html",{
+            "message":"thank u for booking"
+        })
+wanna ask why this does not work
+"""
