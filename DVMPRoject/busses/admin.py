709f56f1ef913dc46990fec9997d70cfbb38a74f
eshs01 <f20241262@pilani.bits-pilani.ac.in>
2025-02-10 01:46:39 +0530

    pretty much everything changed

--- DVMPRoject/busses/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import STATION,BUSSES,Passanger
-admin.site.register(STATION)
-admin.site.register(BUSSES)
-admin.site.register(Passanger)
--- DVMPRoject/busses/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns=[
-    path("",views.index,name="index"),
-    path("<int:bus_id>",views.buz,name="bus")
-    
-]
